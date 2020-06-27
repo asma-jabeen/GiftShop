19ff835eba2de84a2ef81fc9449215f326e17f83
asma jabeen <asma.jabeen@hotmail.com>
2020-06-27 15:13:49 -0700

    added route for home and products page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product/product.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+const appRoutes: Routes = [
+  { path: '', component: ProductComponent },
+  { path: 'products', component: ProductComponent }
+] 
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +19,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
