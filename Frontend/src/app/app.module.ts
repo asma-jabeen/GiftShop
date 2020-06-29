218247d16b52f18d8acbb88505bf9cf9f8ac118c
asma jabeen <asma.jabeen@hotmail.com>
2020-06-29 02:34:00 -0700

    added route for product details page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -13,7 +13,7 @@ const appRoutes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'card-templates', component: CardTemplateComponent },
-  { path: 'card-templates', component: ProductDetailComponent }
+  { path: 'product-detail/:id', component: ProductDetailComponent }
 ] 
 @NgModule({
   declarations: [
